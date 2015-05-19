74a16f82b90ed6fb83a74dcc5d6aff336598b2c3
G. Stewart <gastewserver@hotmail.com>
2015-05-19 10:40:43 -0400

    Added more normality to the generation and implemented it in a test. Weird bug with a double platform being generated, but that's on the list of things to fix. (ONLY does random Y generation not X)

--- Platformer/Platformer/Platformer/GameClass/PlatformHandler.cs
+++ Platformer/Platformer/Platformer/GameClass/PlatformHandler.cs
@@ -19,6 +19,7 @@ namespace Platformer.GameClass
         private List<Vector2> platformPositions;
         private double platformSpeed = 150; //pixels a second
         private Vector2 platformSize = new Vector2(150, 40); //width, height
+        private Vector2 screenSize = new Vector2(1080, 720); //Temp
 
         public PlatformHandler()
         {
@@ -64,26 +65,38 @@ namespace Platformer.GameClass
                 platformPositions[i] = new Vector2(platformPositions[i].X + (float)(platformSpeed * gameTime.ElapsedGameTime.TotalSeconds), platformPositions[i].Y);
 
                 //Test
-                int screenWidth = 1080;
-                if (platformPositions[i].X * drawScale.X > screenWidth)
+                if (platformPositions[i].X * drawScale.X > screenSize.X)
                 {
                     //Replace with random generated
-                    platformPositions[i] = new Vector2(-platformSize.X - 50, 300);
+                    //platformPositions[i] = new Vector2(-platformSize.X - 50, 300);
+                    if (i > 0)
+                        platformPositions[i] = randPlatformPos(new Rectangle((int)platformPositions[i - 1].X, (int)platformPositions[i - 1].Y, (int)platformSize.X, (int)platformSize.Y), 3, 4, screenSize, new Vector2(0, 100));
+                    else
+                        platformPositions[i] = randPlatformPos(new Rectangle((int)platformPositions[i].X, (int)platformPositions[i].Y, (int)platformSize.X, (int)platformSize.Y), 3, 4, screenSize, new Vector2(0, 100));
                 }
             }
         }
 
-        public Vector2 randPlatformPos(Rectangle lastPlatform, int maxPlatVarianceWidth, int maxPlatVarianceHeight)
+        public Vector2 randPlatformPos(Rectangle lastPlatform, int maxPlatVarianceWidth, int maxPlatVarianceHeight, Vector2 screenSize, Vector2 boundSize)
         {
             Vector2 genPos = new Vector2();
             Random rand = new Random();
             int genNum;
 
-            genNum = rand.Next(-maxPlatVarianceWidth, maxPlatVarianceWidth);
-            genPos.X = (genNum * lastPlatform.Width) + lastPlatform.Width;
-            genPos.X = genPos.X - (1080 - genPos.X);
-            genNum = rand.Next(-maxPlatVarianceHeight, maxPlatVarianceHeight);
-            genPos.Y = (genNum * lastPlatform.Height) + lastPlatform.Y + lastPlatform.Height;
+            genNum = rand.Next(-maxPlatVarianceWidth * 3, maxPlatVarianceWidth * 3);
+            genPos.X = -lastPlatform.Width - 50;//(genNum * lastPlatform.Width) + lastPlatform.Width;
+            if(lastPlatform.Y < boundSize.Y + 50)
+                genNum = rand.Next(0, maxPlatVarianceHeight * 3);
+            else if(lastPlatform.Y > screenSize.Y - boundSize.Y - 50)
+                genNum = rand.Next(-maxPlatVarianceHeight * 3, 0);
+            else
+                genNum = rand.Next(-maxPlatVarianceHeight * 3, maxPlatVarianceHeight * 3);
+            genPos.Y = ((genNum / 3) * lastPlatform.Height) + lastPlatform.Y + lastPlatform.Height;
+
+            if (genPos.Y + lastPlatform.Height > screenSize.Y - boundSize.Y)
+                genPos.Y = screenSize.Y - boundSize.Y - lastPlatform.Height;
+            else if (genPos.Y < boundSize.Y)
+                genPos.Y = boundSize.Y;
 
             return genPos;
         }
